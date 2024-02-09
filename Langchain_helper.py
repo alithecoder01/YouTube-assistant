9c6e1f240483db29d39c38d96a881add0ee0c02a
Ali Hasan <capt.3lii@gmail.com>
2024-02-09 14:50:31 +0300

    import the laibraries

---
+++ Langchain_helper.py
@@ -0,0 +1,6 @@
+from langchain.document_loaders import YoutubeLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import openai
+from langchain import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.vectorstores import faiss
